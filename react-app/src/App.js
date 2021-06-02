ca6af690d50135620a686af1497c85d4cf41a4cf
Tonomatic <antonysoly2217@gmail.com>
2021-06-02 18:16:42 -0400

    this is the first commit to this branch

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import LoginForm from "./components/auth/LoginForm";
 import SignUpForm from "./components/auth/SignUpForm";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/index";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
@@ -42,7 +42,6 @@ function App() {
           <User />
         </ProtectedRoute>
         <ProtectedRoute path="/" exact={true} >
-          <h1>My Home Page</h1>
         </ProtectedRoute>
       </Switch>
     </BrowserRouter>
---
+++ react-app/src/components/NavBar/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { NavLink, Redirect } from 'react-router-dom';
+import LogoutButton from '../auth/LogoutButton';
+import "./NavBar.css"
+
+const redirecting = () => {
+  return (
+    <Redirect to="/" />
+  )
+}
+
+const NavBar = () => {
+  return (
+    <nav className="First_nav">
+      <div>
+        <NavLink to="/" exact={true} className="tags" activeClassName="active">
+          Home
+          </NavLink>
+      </div>
+      <div>
+        <NavLink to="/login" exact={true} className="tags" activeClassName="active">
+          Login
+          </NavLink>
+      </div>
+      <div>
+        <NavLink to="/sign-up" exact={true} className="tags" activeClassName="active">
+          Sign Up
+          </NavLink>
+      </div>
+      <div>
+        <NavLink to="/users" exact={true} className="tags" activeClassName="active">
+          Users
+          </NavLink>
+      </div>
+      <div>
+        <LogoutButton />
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
