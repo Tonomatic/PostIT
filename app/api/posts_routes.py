929583be8a48201e2acfa2b774df3b126784f806
Tonomatic <antonysoly2217@gmail.com>
2021-06-03 17:49:29 -0400

    MyPosts

---
+++ app/api/posts_routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import User, Post
+
+posts_routes = Blueprint('posts', __name__)
+
+@posts_routes
+def posts(id):
+    posts = Post.query.filter(Post.userId == id).all()
+    return {"posts": [post.to_dict() for post in posts]}
+
+##This will create a post(question)
+# @posts_routes
+# def createPosts(id):
+#     form =
---
+++ react-app/src/store/post.js
@@ -0,0 +1,30 @@
+// constants
+const GET_POST = "post/GET_POST";
+
+const getPost = (data) => ({
+    type: GET_POST,
+    payload: data
+})
+
+export const myPosts = (userId) => async (dispatch) => {
+    const res = await fetch(`/api/posts/${userId}`, {
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+    if(res.ok) {
+        const data = await res.json();
+        dispatch(getPost(data))
+    }
+}
+
+export default function reducer(state=[], action) {
+    let newState;
+    switch (action.type) {
+        case GET_POST:
+            newState = action.payload.posts
+            return newState;
+        default:
+            return state;
+    }
+}
