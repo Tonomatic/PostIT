85fc357dbd7a602d1dd5861ef21cc1b22e246517
Tonomatic <antonysoly2217@gmail.com>
2021-06-04 19:11:27 -0400

    Starting Friends Front-end

---
+++ react-app/src/components/Friends/index.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect, useParams, NavLink } from "react-router-dom";
+import { myFriends } from "../../store/friend";
+import './Friends.css'
+
+
+const Friends = () => {
+
+
+
+    const user = useSelector(state => state.session.user)
+
+    useEffect(() => {
+        dispatch(myFriends(user.id))
+    }, [dispatch])
+
+
+    return(
+        <div>
+            Friends Route
+            <div>
+                {}
+            </div>
+        </div>
+    )
+}
+
+
+export default Friends
