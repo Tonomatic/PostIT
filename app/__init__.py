5f03e2ec5920491a4cf6c04c075c8c58bedd74d3
jose <antonysoly2217@gmail.com>
2021-06-05 17:45:01 -0400

    Worked on the answers route, a post route and favicon change

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.posts_routes import posts_routes
+from .api.answers_routes import answers_routes
 # from .api.friend_routes import friend_routes
 
 from .seeds import seed_commands
@@ -34,6 +35,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(posts_routes, url_prefix='/api/posts')
+app.register_blueprint(answers_routes, url_prefix='/api/answers')
 # app.register_blueprint(friend_routes, url_prefix='/api/friends')
 db.init_app(app)
 Migrate(app, db)
