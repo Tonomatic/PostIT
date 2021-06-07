aee061a6d3c71b652f7e070e23ac679cb4612a69
Tonomatic <antonysoly2217@gmail.com>
2021-06-06 21:37:12 -0400

    another commit

--- react-app/src/components/Answers/index.js
+++ react-app/src/components/Answers/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect, useParams, NavLink } from "react-router-dom";
-import { myPosts, createPost, noMorePost, myAnswers } from "../../store/post";
+import { myAnswers } from "../../store/post";
 import { createAnswer } from "../../store/answer";
 import './Answers.css'
 
